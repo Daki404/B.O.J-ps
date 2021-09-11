1f66c189c06c479eb6071a8e27cdce1d70331a77
Daki <daki403.1@gmail.com>
2021-09-11 22:59:31 +0900

    7562 나이트 이동문제 해결

--- 7562.py
+++ 7562.py
@@ -1,31 +1,28 @@
 from collections import deque
 import sys
+
 input = sys.stdin.readline
+move_x = [-2, -2, -1, -1, 2, 2, 1, 1]
+move_y = [1, -1, 2, -2, 1, -1, 2, -2]
 
 
-def knight_calc(v, ed, w):
-    if v == ed: return 0
+def knight_calc(st, ed, field):
     queue = deque([st])
-    visit = []
+    field[st[0]][st[1]] = 1
     while queue:
         v = queue.popleft()
-        if v in visit: continue
-        visit.append(v)
-        if st == ed: return (len(visit) - 1) // 8 + 1
-        if (0 <= v[0] and v[0] < w) and (0 <= v[1] and v[1] < w):
-            queue.append((v[0] - 2, v[1] - 1))
-            queue.append((v[0] - 2, v[1] + 1))
-            queue.append((v[0] - 1, v[1] + 2))
-            queue.append((v[0] - 1, v[1] - 2))
-
-            queue.append((v[0] + 2, v[1] - 1))
-            queue.append((v[0] + 2, v[1] + 1))
-            queue.append((v[0] + 1, v[1] + 2))
-            queue.append((v[0] + 1, v[1] - 2))
+        if v == ed: return field[v[0]][v[1]] - 1
+        for i in range(8):
+            new_v = (v[0] + move_y[i]), (v[1] + move_x[i])
+            if 0 <= new_v[0] < len(field) and 0 <= new_v[1] < len(field) and field[new_v[0]][new_v[1]] == 0:
+                queue.append(new_v)
+                field[new_v[0]][new_v[1]] = field[v[0]][v[1]] + 1
+    return 0
 
 
 for _ in range(int(input())):
-    w = int(input())
+    size = int(input())
+    field = [[0] * size for _ in range(size)]
     st = tuple(map(int, input().split()))
     ed = tuple(map(int, input().split()))
-    print(knight_calc(st, ed, w))
+    print(knight_calc(st, ed, field))
