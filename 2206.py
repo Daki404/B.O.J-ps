1c931afd8a27b655fff94a7e9ced4f495277025f
Daki <daki403.1@gmail.com>
2021-09-15 08:43:06 +0900

    2206 벽 부수고 이동하기 해결

--- 2206.py
+++ 2206.py
@@ -1,29 +1,30 @@
 from collections import deque
-import sys
+import sys, copy
 input = sys.stdin.readline
 
 
-def sol(x=0, y=0, stave=True):
-    field[0][0] = 2
-    queue = deque([(x, y, stave)])
+def sol(x=0, y=0, z=0):
+    field[0][0][0] = 2
+    queue = deque([(x, y, z)])
     while queue:
-        x, y, stave = queue.popleft()
-        if x == w - 1 and y == h - 1: return field[y][x] - 1
+        x, y, z = queue.popleft()
         for i, j in zip([0, 0, -1, 1], [1, -1, 0, 0]):
             new_x, new_y = x + i, y + j
             if 0 <= new_x < w and 0 <= new_y < h:
-                if (now := field[new_y][new_x]) == 0:
-                    field[new_y][new_x] = field[y][x] + 1
-                    queue.append((new_x, new_y, stave))
-                elif now == 1 and stave:
-                    field[new_y][new_x] = field[y][x] + 1
-                    queue.append((new_x, new_y, not stave))
-    return -1
+                if (now := field[z][new_y][new_x]) == 0:
+                    field[z][new_y][new_x] = field[z][y][x] + 1
+                    queue.append((new_x, new_y, z))
+                elif now == 1 and z == 0:
+                    field[1][new_y][new_x] = field[0][y][x] + 1
+                    queue.append((new_x, new_y, 1))
+    a, b = field[0][h-1][w-1], field[1][h-1][w-1]
+    if a == 0 or b == 0: return max(a, b) - 1
+    return min(a, b) - 1
 
 
 h, w = map(int, input().split())
 field = []
 for i in range(h):
     field.append(list(map(int, list(input().strip()))))
-
+field = [field, copy.deepcopy(field)]
 print(sol())
