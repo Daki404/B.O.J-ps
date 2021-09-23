a3e500a41bea0116eaab7766a7d3beebb3294334
Daki <daki403.1@gmail.com>
2021-09-23 23:23:05 +0900

    파일 합치기 해결중..

--- 11066.py
+++ 11066.py
@@ -1,10 +1,18 @@
-import sys
+import sys, math
 input = sys.stdin.readline
 
 
 for _ in range(int(input())):
     n = int(input())
     book = list(map(int, input().split()))
-
-    dp = [0] * (n + 1)
-    for i in range(1, )
+    dp = [[math.inf] * n for _ in range(n)]
+    for i, j in enumerate(book):
+        dp[i][i] = j
+    for i in range(n):
+        for j in range(n - i):
+            for k in range(j, i + j):
+                if i <= 1:
+                    dp[j][i + j] = min(dp[j][i + j], dp[j][k] + dp[k + 1][i + j])
+                else:
+                    dp[j][i + j] = min(dp[j][i + j], (dp[j][k] + dp[k + 1][i + j]) * 2)
+    print(dp[0][-1])
